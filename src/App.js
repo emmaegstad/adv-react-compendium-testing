072dbbd97a87eed9ed0f4ae7ff4dd2a698089caa
Emma Egstad <emma@egstad.com>
2022-01-25 12:19:23 -0800

    connecting to API and loading images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [pictures, setPictures] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const rawData = await fetch(
+        'https://api.nasa.gov/planetary/apod?api_key=YunY96dmRe6pkaOAen1SKJJPUFEd2a7TbMPGntRj&count=10'
+      );
+      const data = await rawData.json();
+      setPictures(data);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {pictures.map((picture) => {
+        return <img key={picture.id} src={picture.url} className="image" />;
+      })}
     </div>
   );
 }
